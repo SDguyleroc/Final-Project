a9b715ec07ac613aea1f9bee52e8ffa844fa42de
SDguyleroc <thonerguyleroc1@gmail.com>
2023-04-26 17:28:39 -0400

    added data in the dataContext class using the modelbuilder.Entity

--- FinalProject/Controllers/AddressController.cs
+++ FinalProject/Controllers/AddressController.cs
@@ -1,11 +1,113 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FinalProject.Data;
+using FinalProject.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinalProject.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class AddressController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public AddressController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Address
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Address>>> GetAddresses(int id)
+        {
+            var addresses = _context.Addresses.AsQueryable();
+
+            if (id == null || id == 0)
+            {
+                addresses = addresses.Take(5);
+            }
+            else
+            {
+                addresses = addresses.Where(a => a.Id == id);
+            }
+
+            return await addresses.ToListAsync();
+        }
+
+        // GET: api/Address/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Address>> GetAddress(int id)
+        {
+            var address = await _context.Addresses.FindAsync(id);
+
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return address;
+        }
+
+        // POST: api/Address
+        [HttpPost]
+        public async Task<ActionResult<Address>> PostAddress(Address address)
+        {
+            _context.Addresses.Add(address);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAddress), new { id = address.Id }, address);
+        }
+
+        // PUT: api/Address/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAddress(int id, Address address)
+        {
+            if (id != address.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(address).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AddressExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Address/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAddress(int id)
+        {
+            var address = await _context.Addresses.FindAsync(id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            _context.Addresses.Remove(address);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AddressExists(int id)
+        {
+            return _context.Addresses.Any(e => e.Id == id);
+        }
     }
 }
--- FinalProject/Data/DataContext.cs
+++ FinalProject/Data/DataContext.cs
@@ -42,6 +42,125 @@ namespace FinalProject.Data
             modelBuilder.Entity<FavoriteBreakFast>()
         .Property(p => p.Price)
         .HasPrecision(18, 2);
+
+            modelBuilder.Entity<TeamMember>().HasData(
+        new TeamMember
+        {
+            Id = 1,
+            FirstName = "John",
+            LastName = "Doe",
+            BirthDate = new DateTime(1990, 1, 1),
+            CollegeProgram = "Computer Science",
+            YearInProgram = "Junior"
+        },
+        new TeamMember
+        {
+            Id = 2,
+            FirstName = "Jane",
+            LastName = "Doe",
+            BirthDate = new DateTime(1995, 1, 1),
+            CollegeProgram = "Engineering",
+            YearInProgram = "Sophomore"
+        }
+    );
+            modelBuilder.Entity<FavoriteBreakFast>().HasData(
+            new FavoriteBreakFast
+            {
+                Id = 1,
+                Name = "Blueberry Pancakes",
+                Description = "Fluffy pancakes loaded with fresh blueberries",
+                Category = "Breakfast",
+                Price = 6.99m,
+                TeamMemberId = 1
+            },
+        new FavoriteBreakFast
+        {
+            Id = 2,
+            Name = "Avocado Toast",
+            Description = "Toasted bread topped with mashed avocado and seasoning",
+            Category = "Breakfast",
+            Price = 8.99m,
+            TeamMemberId = 2
+        }
+    );
+            modelBuilder.Entity<Hobby>().HasData(
+            new Hobby
+            {
+                Id = 1,
+                Name = "Reading",
+                Description = "Enjoying a good book",
+                Category = "Leisure",
+                Frequency = "Daily"
+            },
+            new Hobby
+            {
+                Id = 2,
+                Name = "Running",
+                Description = "Jogging outdoors",
+                Category = "Fitness",
+                Frequency = "Weekly"
+            },
+            new Hobby
+            {
+                Id = 3,
+                Name = "Gardening",
+                Description = "Growing plants and flowers",
+                Category = "Hobby",
+                Frequency = "Monthly"
+            }
+        );
+
+            modelBuilder.Entity<TeamMemberHobby>().HasData(
+                new TeamMemberHobby
+                {
+                    TeamMemberId = 1,
+                    HobbyId = 1
+                },
+                new TeamMemberHobby
+                {
+                    TeamMemberId = 2,
+                    HobbyId = 2
+                },
+                new TeamMemberHobby
+                {
+                    TeamMemberId = 3,
+                    HobbyId = 3
+                }
+            );
+
+            modelBuilder.Entity<Address>().HasData(
+           new Address
+           {
+               Id = 1,
+               Street = "123 Main St",
+               City = "Anytown",
+               State = "CA",
+               PostalCode = "12345",
+               Country = "USA",
+               TeamMemberId = 1
+           },
+           new Address
+           {
+               Id = 2,
+               Street = "456 Elm St",
+               City = "Anytown",
+               State = "CA",
+               PostalCode = "12345",
+               Country = "USA",
+               TeamMemberId = 2
+           },
+           new Address
+           {
+               Id = 3,
+               Street = "789 Oak St",
+               City = "Anytown",
+               State = "CA",
+               PostalCode = "12345",
+               Country = "USA",
+               TeamMemberId = 3
+           }
+       );
+
         }
     }
 }
