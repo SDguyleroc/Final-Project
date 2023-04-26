b1ce55e3d80824915789c75e3d2014671374ab2e
ossebi-gc <79544566+ossebi-gc@users.noreply.github.com>
2023-04-25 20:00:05 -0400

    added a primary key in teamMemberHobby.

---
+++ FinalProject/Controllers/TeamMemberController.cs
@@ -0,0 +1,116 @@
+﻿using FinalProject.Data;
+using FinalProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TeamMembersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public TeamMembersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TeamMembers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TeamMember>>> GetTeamMembers(int? id = null)
+        {
+            var query = _context.TeamMembers.AsQueryable();
+
+            if (id.HasValue && id.Value != 0)
+            {
+                query = query.Where(tm => tm.Id == id.Value);
+            }
+            else
+            {
+                query = query.Take(5);
+            }
+
+            return await query.ToListAsync();
+        }
+
+        // GET: api/TeamMembers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TeamMember>> GetTeamMember(int id)
+        {
+            var teamMember = await _context.TeamMembers.FindAsync(id);
+
+            if (teamMember == null)
+            {
+                return NotFound();
+            }
+
+            return teamMember;
+        }
+
+        // PUT: api/TeamMembers/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTeamMember(int id, TeamMember teamMember)
+        {
+            if (id != teamMember.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(teamMember).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TeamMemberExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TeamMembers
+        [HttpPost]
+        public async Task<ActionResult<TeamMember>> PostTeamMember(TeamMember teamMember)
+        {
+            _context.TeamMembers.Add(teamMember);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTeamMember), new { id = teamMember.Id }, teamMember);
+        }
+
+        // DELETE: api/TeamMembers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTeamMember(int id)
+        {
+            var teamMember = await _context.TeamMembers.FindAsync(id);
+            if (teamMember == null)
+            {
+                return NotFound();
+            }
+
+            _context.TeamMembers.Remove(teamMember);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TeamMemberExists(int id)
+        {
+            return _context.TeamMembers.Any(tm => tm.Id == id);
+        }
+    }
+}
+
--- FinalProject/Models/TeamMeberHobby.cs
+++ FinalProject/Models/TeamMeberHobby.cs
@@ -4,6 +4,8 @@
     {
         public class TeamMemberHobby
         {
+            public int Id { get; set; } 
+
             public int TeamMemberId { get; set; }
             public TeamMember TeamMember { get; set; }
 
